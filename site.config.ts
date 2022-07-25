dfb92bc54e7542d81828abbaedc2e4c55f0191db
猛寇龙崽 <35446259+mklz-sjy@users.noreply.github.com>
2022-07-25 20:24:42 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '771b682dd5364775a04f2f853541eaa9',
+  rootNotionPageId: '72d8e553617d4ac8b42ce29616341f4a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'mklz_blog',
-  domain: 'https://humane-universe-f71.notion.site/Blog-771b682dd5364775a04f2f853541eaa9',
+  domain: 'https://humane-universe-f71.notion.site/Notes-72d8e553617d4ac8b42ce29616341f4a',
   author: 'mklz',
 
   // open graph metadata (optional)
